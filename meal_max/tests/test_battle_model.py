bd57d70bd46a230a0ba601c451c3ce970b827a38
Lily Beaudion <beaudion@bu.edu>
2024-11-01 08:33:35 -0400

    drafts of all battle_model unit tests

--- meal_max/tests/test_battle_model.py
+++ meal_max/tests/test_battle_model.py
@@ -30,11 +30,19 @@ def sample_combatants(sample_meal1, sample_meal2):
 
 
 # test battle
-def test_battle(battle_model):
+def test_battle(battle_model, sample_combatants):
     """Test battle."""
+    battle_model.combatants.extend(sample_combatants)
+
+    winner = battle_model.battle()
+    assert len(sample_combatants) == 1
+    assert winner == winner.meal
 
 # test battle, less than 2 combatants
+def test_battle_less_2_combatants(battle_model, sample_meal1):
+    battle_model.combatants.extend(sample_meal1)
 
+    assert len(battle_model.combatants) < 2, "Expected 1 combatant"
 
 
 # test clear combatants
@@ -54,9 +62,37 @@ def test_clear_combatants_empty_combatants(battle_model, caplog):
 
 
 # test get battle score
+def test_get_battle_score(battle_model, sample_meal1):
+    """Test getting the battle score of a combatant"""
+    battle_model.prep_combatant(sample_meal1)
+
+    score = battle_model.get_battle_score(sample_meal1)
+    assert score == 31
+
 
 # test get combatants
+def test_get_combatants(battle_model, sample_combatants):
+    '''Test getting combatants'''
+    battle_model.combatants.extend(sample_combatants)
+
+    all_combatants = battle_model.get_combatants()
+    assert len(all_combatants) == 2
+    assert all_combatants[0].id == 1
+    assert all_combatants[1].id == 2
+
 
 # test prep combatants
+def test_prep_combatants(battle_model, sample_meal1):
+    """Test adding a combatant to the combatants list"""
+    battle_model.prep_combatant(sample_meal1)
+    assert len(battle_model.combatants) == 1
+    assert battle_model.combatants[0].meal == 'Meal 1'
+
 
 # test prep combatants, adding more than 2 combatants
+def test_prep_combatants_more_2_combatants(battle_model, sample_combatants, sample_meal1):
+    """Test adding a combatant to an already full combatants list"""
+    battle_model.combatants.extend(sample_combatants)
+    battle_model.prep_combatant(sample_meal1)
+    with pytest.raises(ValueError, match="Combatant list is full, cannot add more combatants."):
+        battle_model.prep_combatant(sample_meal1)
